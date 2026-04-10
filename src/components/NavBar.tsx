fdc17b11aeaef584520e9bd6c1515b57880f2be6
Bgisp <spa.giaio981@gmail.com>
2026-04-10 23:36:54 +0200

    fix path cv

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -71,7 +71,7 @@ function NavBar({ sections, active, onSelect }: NavBarProps) {
               </a>
             ))}
             <a
-            href="/portfolio/src/assets/Biagio_Spada_Resume_CV.pdf"
+            href={`${import.meta.env.BASE_URL}cv/Biagio_Spada_CV.pdf`}
               target="_blank"
               rel="noopener noreferrer"
               style={{
